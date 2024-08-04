25d2dd10c803b492cbe1ac464dd905972cb7121a
TimBik <timurbikmullin@mail.ru>
2024-08-04 19:52:58 +0300

    update fields description

--- bboard/views.py
+++ bboard/views.py
@@ -7,5 +7,5 @@ from bboard.models import Bb
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs': bbs})
