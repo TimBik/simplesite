2e5349b9e9b849769ebe7a833934e2e880c8d68e
TimBik <timurbikmullin@mail.ru>
2024-08-04 03:52:20 +0300

    add Bb model

--- bboard/models.py
+++ bboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
