b9404dfbf7f3a339f5c6c47647f1199c2f4ca752
TimBik <timurbikmullin@mail.ru>
2024-08-04 03:52:07 +0300

    update urls

--- simplesite/urls.py
+++ simplesite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import bboard.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', bboard.views.index),
+    path('index/', include('bboard.urls')),
 ]
